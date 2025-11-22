76e58ec43dfba1b2f3b4d3ae007dd3797591d331
Sathya-Anavarathan <sathyaanavarathan10@gmail.com>
2025-11-22 22:56:53 +0530

    add error handiling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import connectDB from "./src/config/db.js";
 import recipeRouter from "./src/routes/recipeRouter.js";
+import errorHandler from './src/middlewares/errorHandler.js';
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,8 @@ connectDB();
 // All recipe routes
 app.use("/api/recipe", recipeRouter);
 
+
+app.use(errorHandler)
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
